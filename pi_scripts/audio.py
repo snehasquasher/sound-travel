cf974bb53b21871fd3468ee38d12a64c7f53fd18
Sneha <sneha.squasher@gmail.com>
2023-04-30 13:56:59 -0400

    made changes on audio.py, added functionality for integrating potentiometer data

---
+++ pi_scripts/audio.py
@@ -0,0 +1,95 @@
+import pygame
+# import RPi.GPIO as GPIO
+import csv
+# module for handling sound in a python program 
+
+#Initializing pygame mixer module 
+pygame.mixer.init() 
+
+#define audio files + associated locations on the world map 
+#creating sound_directory  
+with open('recordings.csv','r') as file: 
+    reader = csv.reader(file)
+    #skip the header row 
+    next(reader)
+    
+    #create dict to store data 
+    sound_directory = {}
+    
+    #traverse each row in the CSV file and add them to the dict 
+    for row in reader: 
+        key = row[6]
+        values = [float(row[4]),float(row[5])]
+        
+        print(values)
+        sound_directory[key] = values
+print(len(sound_directory))
+        
+#set-up the potentiometers
+# pot1_pin = 18
+# pot2_pin = 23
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(pot1_pin, GPIO.IN)
+# GPIO.setup(pot2_pin, GPIO.IN)
+
+# function mapping potentiometer values to longitude and latitude
+def map_potentiometer_values(pot1_value, pot2_value):
+    # Map the potentiometer values to longitude and latitude
+    longitude = pot1_value * 0.01 - 180.0
+    latitude = pot2_value * 0.01 - 90.0
+    # Return the mapped longitude and latitude
+    return longitude, latitude
+
+# function to find the nearest audio file based on longitude and latitude
+def find_nearest_audio_file(longitude, latitude):
+    # Find the nearest audio file based on the distance from each location on the world map
+    nearest_location = None
+    nearest_distance = float("inf")
+    for name,location_data in sound_directory.items():
+        #using the shortest distance formula --> hopefully this works 
+        distance = ((location_data[0] - longitude) ** 2 + (location_data[1] - latitude) ** 2) ** 0.5
+        if distance < nearest_distance:
+            nearest_location = name
+            nearest_distance = distance
+    # Return the audio file corresponding to the nearest location
+    return nearest_location
+
+# Main loop
+while True:
+    # Read the potentiometer values
+    # pot1_value = GPIO.input(pot1_pin)
+    # pot2_value = GPIO.input(pot2_pin)
+    # Map the potentiometer values to longitude and latitude
+    longitude, latitude = map_potentiometer_values(50, 80)
+    # Find the nearest audio file based on longitude and latitude
+    nearest_location = find_nearest_audio_file(longitude, latitude)
+    # Load and play the audio file
+    break
+    # sound = pygame.mixer.Sound(audio_file)
+    # sound.play()
+
+
+
+# ORIGINAL CODE 
+# # Create the I2C bus
+# i2c = busio.I2C(board.SCL, board.SDA)
+
+# # Create the ADC object using the I2C bus
+# ads = ADS.ADS1015(i2c)
+
+# # Create single-ended input on channel 0
+# chan = AnalogIn(ads, ADS.P0)
+# # chan2 = AnalogIn(ads, ADS.P1)
+
+# # Create differential input between channel 0 and 1
+# #chan = AnalogIn(ads, ADS.P0, ADS.P1)
+
+# # Test Print
+# print("{:>5}\t{:>5}".format('raw', 'v'))
+# #load the sound giles 
+# pygame.mixer.music.load("pi_scripts/test.wav")
+# pygame.mixer.music.play()
+# while pygame.mixer.music.get_busy() == True:
+#     continue
+
+
